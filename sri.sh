82d5d2eba8b9a3c8d06201541b34e40af274cdd5
gunnamsri <sekhargunnam5270@gmail.com>
2018-08-03 07:29:33 -0400

    first commit

--- sri.sh
+++ sri.sh
@@ -3,8 +3,11 @@ int1=$1
 int2=$2
 if [ $int1 -eq $int2 ]; then
 echo "values are equal"
+elif [ $int1 -gt $int2 ];then
+echo "values are grater"
 else
-echo "values are not equal"
+echo "values are not valid"
 fi
+
 
 
